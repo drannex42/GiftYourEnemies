ea230257585c4762f2e57bb5c24a97c367fc110a
Macleod Sawyer <macleod@charitydrops.org>
2015-01-21 01:22:53 -0600

    added paypal options
    Added paypal

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '' });
+});
+
+/* GET home page. */
+router.get('/paypal', function(req, res) {
+  res.render('paypal', { title: 'Pay With Paypal - Gift Your Enemies' });
 });
 
 module.exports = router;
